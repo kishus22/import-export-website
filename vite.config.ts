cbc3f4b7db040833cfaa24440cae6e827fd755a5
kishus22 <kishan.s.220803@gmail.com>
2025-12-02 16:05:02 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
+const base = process.env.VITE_BASE || '/';
+
 export default defineConfig({
+  base,               // allows conditional base for GH Pages vs Vercel
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
